3af2d15bd0344115dd55ca5e22b2020828e9a586
carolynworldwide <carolynworldwide@gmail.com>
2015-09-30 16:10:48 -0700

    second commit

--- game.js
+++ game.js
@@ -1,15 +1,28 @@
-
   var username = prompt('What is your name?');
     alert('Good to meet you, ' + username);
-  var username = prompt('Did I grow up in the United States');
-    alert('Yes');
-    alert('No');
-  var username = prompt('Do I speak German');
-    alert('Yes');
-    alert('No');
-  var username = prompt('Have I ever lived in Libya');
-    alert('Yes');
-    alert('No');
-  var username = prompt("You're finished")
-    alert('Congrats!')
-
+  
+  var question1 = prompt('Did I grow up in the United States?');
+    if (question1.toUpperCase() === 'Y' || question1.toUpperCase() === 'YES') {
+      alert('Right!');
+    }
+    else {
+      alert('Wrong!');
+    }
+  
+  var question2 = prompt('Do I speak German?');
+    if (question2.toUpperCase() === 'N' || question2.toUpperCase() === 'NO') {
+      alert('Correct!');
+    }
+    else {
+      alert('Sorry!');
+    }
+  
+  var question3 = prompt('Have I ever lived in Libya');
+    if (question3.toUpperCase() === 'Y' || question3.toUpperCase() === 'YES') {
+      alert('Indeed!');
+    }
+    else {
+      alert('Actually I did!');
+    }
+  
+  var closing = alert("Finished!");
