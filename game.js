5d6a65c12a88371974314da76ec063ab08d41f22
carolynworldwide <carolynworldwide@gmail.com>
2015-09-30 16:31:17 -0700

    third commit

--- game.js
+++ game.js
@@ -1,9 +1,11 @@
+var totalRight = 0;
   var username = prompt('What is your name?');
     alert('Good to meet you, ' + username);
   
   var question1 = prompt('Did I grow up in the United States?');
     if (question1.toUpperCase() === 'Y' || question1.toUpperCase() === 'YES') {
       alert('Right!');
+      totalRight++;
     }
     else {
       alert('Wrong!');
@@ -12,6 +14,7 @@
   var question2 = prompt('Do I speak German?');
     if (question2.toUpperCase() === 'N' || question2.toUpperCase() === 'NO') {
       alert('Correct!');
+      totalRight++;
     }
     else {
       alert('Sorry!');
@@ -20,9 +23,12 @@
   var question3 = prompt('Have I ever lived in Libya');
     if (question3.toUpperCase() === 'Y' || question3.toUpperCase() === 'YES') {
       alert('Indeed!');
+      totalRight++;
     }
     else {
       alert('Actually I did!');
     }
   
-  var closing = alert("Finished!");
+  var closing = alert('Finished! You got ' + totalRight + ' right ' + username);
+
+console.log(totalRight)
