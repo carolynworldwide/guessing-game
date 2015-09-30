74d8dd8b9d75ee17302ad76f93a60cde40cc428f
carolynworldwide <carolynworldwide@gmail.com>
2015-09-30 14:25:38 -0700

    initial commit

---
+++ game.js
@@ -0,0 +1,15 @@
+
+  var username = prompt('What is your name?');
+    alert('Good to meet you, ' + username);
+  var username = prompt('Did I grow up in the United States');
+    alert('Yes');
+    alert('No');
+  var username = prompt('Do I speak German');
+    alert('Yes');
+    alert('No');
+  var username = prompt('Have I ever lived in Libya');
+    alert('Yes');
+    alert('No');
+  var username = prompt("You're finished")
+    alert('Congrats!')
+
