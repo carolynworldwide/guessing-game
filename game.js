023c22980500d883580e81506ac7c642e259dfe9
DonChatelain <djchatelain@gmail.com>
2015-10-02 16:51:45 -0700

    cleaned up code

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 var totalRight = 0;
+
 var username = prompt('What is your name?');
     alert('Good to meet you, ' + username);
  
@@ -19,7 +20,7 @@ var elImg3 = document.getElementById('img-contain3');
 
 var elResults = document.getElementById('results');
 
-function question1(){
+function question1() {
 	question1 = prompt(questions[0]);
 	if (question1.toUpperCase() === answers[0] || question1.toUpperCase() === answers[0][0]) {
   		elImg1.innerHTML = "<img src='img/success-1.jpg' />";
@@ -27,7 +28,7 @@ function question1(){
   		one.innerHTML = 'Right!';
   		//alert('Right!');
   		totalRight++;
-	}else {
+	} else {
 		one.className = 'incorrect';
 		one.innerHTML = 'Wrong!';
   		//alert('Wrong!');
@@ -35,7 +36,7 @@ function question1(){
 }
 
 
-function question2(){
+function question2() {
 	question2 = prompt(questions[1]);
 	if (question2.toUpperCase() === answers[1] || question2.toUpperCase() === answers[1][0]) {
   		//alert('Correct!');
@@ -43,7 +44,7 @@ function question2(){
   		two.className = 'correct';
   		two.innerHTML = 'Correct!';
   		totalRight++;
-	}else {
+	} else {
 		two.className = 'incorrect';
 		two.innerHTML = 'Sorry!';
   		//alert('Sorry!');
@@ -51,7 +52,7 @@ function question2(){
 }
 
 
-function question3(){
+function question3() {
 	question3 = prompt(questions[2]);
 	if (question3.toUpperCase() === answers[0] || question3.toUpperCase() === answers[0][0]) {
 		elImg3.innerHTML = "<img src='img/success-3.jpg' />";
@@ -59,7 +60,7 @@ function question3(){
   		three.innerHTML = 'Indeed!';
   		//alert('Indeed!');
   		totalRight++;
-	}else {
+	} else {
 		three.className = 'incorrect';
 		three.innerHTML = 'Actually I did!';
   		//alert('Actually I did!');
