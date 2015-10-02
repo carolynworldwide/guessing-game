eac43f2d67ddabcb501e528858d1bc860478a57c
DonChatelain <djchatelain@gmail.com>
2015-10-02 16:43:44 -0700

    add green to correct response, add red to incorrect response

--- game.js
+++ game.js
@@ -23,11 +23,13 @@ function question1(){
 	question1 = prompt(questions[0]);
 	if (question1.toUpperCase() === answers[0] || question1.toUpperCase() === answers[0][0]) {
   		elImg1.innerHTML = "<img src='img/success-1.jpg' />";
-  		one.innerHTML = 'Right!'
+  		one.className = 'correct';
+  		one.innerHTML = 'Right!';
   		//alert('Right!');
   		totalRight++;
 	}else {
-		one.innerHTML = 'Wrong!'
+		one.className = 'incorrect';
+		one.innerHTML = 'Wrong!';
   		//alert('Wrong!');
 	}
 }
@@ -37,11 +39,13 @@ function question2(){
 	question2 = prompt(questions[1]);
 	if (question2.toUpperCase() === answers[1] || question2.toUpperCase() === answers[1][0]) {
   		//alert('Correct!');
-  		elImg2.innerHTML = "<img src='img/success-2.jpg' />"
-  		two.innerHTML = 'Correct!'
+  		elImg2.innerHTML = "<img src='img/success-2.jpg' />";
+  		two.className = 'correct';
+  		two.innerHTML = 'Correct!';
   		totalRight++;
 	}else {
-		two.innerHTML = 'Sorry!'
+		two.className = 'incorrect';
+		two.innerHTML = 'Sorry!';
   		//alert('Sorry!');
 	}
 }
@@ -50,11 +54,13 @@ function question2(){
 function question3(){
 	question3 = prompt(questions[2]);
 	if (question3.toUpperCase() === answers[0] || question3.toUpperCase() === answers[0][0]) {
-		elImg3.innerHTML = "<img src='img/success-3.jpg' />"
+		elImg3.innerHTML = "<img src='img/success-3.jpg' />";
+		three.className = 'correct';
   		three.innerHTML = 'Indeed!';
   		//alert('Indeed!');
   		totalRight++;
 	}else {
+		three.className = 'incorrect';
 		three.innerHTML = 'Actually I did!';
   		//alert('Actually I did!');
 	}
